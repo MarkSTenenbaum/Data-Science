96f349a2521bcd55e7dac3f8fb06cb8f2222003e
MarkSTenenbaum <112495365+MarkSTenenbaum@users.noreply.github.com>
2024-05-29 08:54:58 -0400

    Update and rename Factor_Analysis.R to Factor_Analysis_and_PCA.R

---
+++ Factor_Analysis_and_PCA.R
@@ -0,0 +1,19 @@
+## Exploratory factor analysis example
+library(psych) # To run EFA
+library(REdaS) # KMO & Bartletts test
+
+econ16 <- sd16 %>% dplyr::select(AMU304, AMU302, AMU305, AMU303, AMU301)
+
+KMO(econ16)
+bart_spher(econ16, use = 'complete.obs')
+fa(econ16, nfactors = ncol(econ16), rotate = 'oblimin') # 1
+
+## PCA Example
+library(psych)
+relevant_data <- c20[, c("gsamb", "abortamb", "ambecon")]
+pca_result <- principal(relevant_data, nfactors = 1, rotate = "none"); pca_result
+
+loadings <- pca_result$loadings
+weights <- loadings[,1]
+scale_scores <- as.matrix(relevant_data) %*% weights   # create weighted scale
+
